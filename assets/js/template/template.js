e5428db2d9cae94ee4216b1d09a6028c3924367d
Raff <rafiazhim.ra@gmail.com>
2023-12-01 20:06:04 +0700

    update

---
+++ assets/js/template/template.js
@@ -0,0 +1,4 @@
+export let UrlRegister = `https://asia-southeast2-argon-fire-401902.cloudfunctions.net/postRegisterEDUMAS`
+export let UrlLogin = `https://asia-southeast2-argon-fire-401902.cloudfunctions.net/postLoginEDUMAS`
+export let UrlSubmitReport ='https://asia-southeast2-argon-fire-401902.cloudfunctions.net/postReportEDUMAS'
+export let token = 'token';
