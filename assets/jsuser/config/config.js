ad22413bd4ad61e063afbebcc2032048e7f3d078
miqbalramadhan18 <miqbalr18@gmail.com>
2024-01-12 18:57:39 +0700

    change link gcp login

--- assets/jsuser/config/config.js
+++ assets/jsuser/config/config.js
@@ -2,7 +2,7 @@ import { setCookieWithExpireHour } from 'https://jscroot.github.io/cookie/croot.
 
 //token
 export function getTokenFromAPI() {
-  const tokenUrl = "https://asia-southeast2-gisiqbal.cloudfunctions.net/User-Login";
+  const tokenUrl = "https://asia-southeast2-gisiqbal.cloudfunctions.net/Login-User";
   fetch(tokenUrl)
     .then(response => response.json())
     .then(tokenData => {
