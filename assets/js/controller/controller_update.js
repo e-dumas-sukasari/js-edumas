41a9c7fa7f5427d3ed1f4ca6c4f4588bd8de5df9
miqbalramadhan18 <miqbalr18@gmail.com>
2024-01-15 10:30:46 +0700

    update lgi fix error update

--- assets/js/controller/controller_update.js
+++ assets/js/controller/controller_update.js
@@ -98,6 +98,7 @@ const getTokenFromCookies = (cookieName) => {
         nik: parseInt(document.getElementById('NikInput').value),
         nama: document.getElementById('NamaInput').value,
         title: document.getElementById('TitleInput').value,
+        description: document.getElementById('DescriptionInput').value,
         DateOccurred: document.getElementById('DateOccurredInput').value,
         image: document.getElementById('ImageInput').value,
         status: statusValue,
