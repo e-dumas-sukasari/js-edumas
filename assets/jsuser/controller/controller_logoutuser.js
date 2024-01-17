2afb2811ee8a9550635981ba3ea2802bb0185107
miqbalramadhan18 <miqbalr18@gmail.com>
2024-01-17 19:16:21 +0700

    add logout

--- assets/jsuser/controller/controller_logoutuser.js
+++ assets/jsuser/controller/controller_logoutuser.js
@@ -2,7 +2,7 @@ import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 function logout() {
   deleteCookie("Login");
-  window.location.href = "https://e-dumas-sukasari.my.id/sign/login.html";
+  window.location.href = "https://e-dumas-sukasari.my.id/sign/login_user.html";
 }
 
 document.getElementById("logout").addEventListener("click", logout);
